ac308c5e56e0badc519bafc3c062780bf3bfd4fb
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-06-24 20:57:10 -0300

    style(notfound): estilização da página

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,8 +1,14 @@
+import { Link } from 'react-router-dom'
+import styles from './notfound.module.css'
 
 export function NotFound(){
     return(
-        <div>
-            <h1>Erro 404!</h1>
+        <div className={styles.container}>
+            <section className={styles.notfound}>
+                <h1 className={styles.title}>Erro 404!</h1>
+                <p className={styles.text}>Ops... essa página não existe!</p>
+                <Link to={'/'} className={styles.btn_voltar}>Voltar a Home</Link>
+            </section>
         </div>
     )
 }
